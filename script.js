1ec32879cb05b1450560db75b51f66ed37ed6634
Muhammad Dlewati <135707988+Muhammad-Dlewati@users.noreply.github.com>
2023-09-02 17:08:27 +0300

    script.js

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let input = document.querySelector("#input");
 let btn = document.querySelector("#btn");
 let h1 = document.querySelector("h1");
 
+window.addEventListener("DOMContentLoaded", () => {
+  document.body.style.backgroundColor = getRandomColor();
+  input.value = document.body.style.backgroundColor;
+});
+
 btn.addEventListener("click", changeBG);
 
 function changeBG() {
@@ -23,5 +28,17 @@ function changeBG() {
       btn.style.backgroundColor = "#000";
       btn.style.color = "#fff";
     }
+  } else {
+    document.body.style.backgroundColor = getRandomColor();
+    input.value = document.body.style.backgroundColor;
   }
+}
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
