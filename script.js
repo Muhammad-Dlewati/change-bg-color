61e0cda6a232b3a4e461ebcd6b17bed792c5ff7d
Muhammad Dlewati <135707988+Muhammad-Dlewati@users.noreply.github.com>
2023-09-02 13:11:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let input = document.querySelector("#input");
+let btn = document.querySelector("#btn");
+let h1 = document.querySelector("h1");
+
+btn.addEventListener("click", changeBG);
+
+function changeBG() {
+  let bgColor = input.value;
+  if (bgColor) {
+    document.body.style.backgroundColor = bgColor;
+    input.value = "";
+
+    if (
+      document.body.style.backgroundColor === "black" ||
+      document.body.style.backgroundColor === "#000"
+    ) {
+      h1.style.color = "#fff";
+      btn.style.backgroundColor = "#fff";
+      btn.style.color = "#000";
+    } else {
+      h1.style.color = "#000";
+      btn.style.backgroundColor = "#000";
+      btn.style.color = "#fff";
+    }
+  }
+}
